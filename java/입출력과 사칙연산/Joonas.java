b1ae6d1a953f6ae7b500036740cd0f943a74d85f
changho-y <83066788+changho-y@users.noreply.github.com>
2025-11-10 02:29:56 +0900

    준하는 사이트에 회원가입을 하다가 joonas라는 아이디가 이미 존재하는 것을 보고 놀랐다. 준하는 놀람을 ??!로 표현한다. 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어졌을 때, 놀람을 표현하는 프로그램을 작성하시오.

---
+++ java/입출력과 사칙연산/Joonas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Joonas {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.print(id+"??!");
+
+	}
+
+}
