2f0766718e584ab83397fb52a261f5c739f97de9
changho-y <83066788+changho-y@users.noreply.github.com>
2025-12-29 23:53:42 +0900

    업데이트

--- 바구니뒤집기.java
+++ 바구니뒤집기.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public void static main
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
