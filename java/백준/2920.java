2f8fda6b6bdba9e8f7dfde0528546d6383e17818
changho-y <83066788+changho-y@users.noreply.github.com>
2026-02-24 22:11:37 +0900

    Create 백준 : 2920

---
+++ java/백준/2920.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int[] arr = new int[8];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			
+			for(int i=0; i<8; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			boolean asc = true;
+			boolean desc = true;
+			
+			for(int i=0; i<7; i++) {
+				if(arr[i+1] > arr[i]) {
+					desc = false;
+				}
+				if(arr[i+1] < arr[i]) {
+					asc = false;
+				}
+			}
+			if(asc) {
+				System.out.print("ascending");
+			} else if(desc) {
+				System.out.print("descending");
+			} else {
+				System.out.print("mixed");
+			}
+			
+		}
+}
